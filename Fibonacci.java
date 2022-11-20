e1082083ba899571f2f92524b039ced6f9c9b2b9
SaiKumar <bsainaidu90@gmail.com>
2022-11-20 22:22:55 +0530

    second commit

---
+++ Fibonacci.java
@@ -0,0 +1,30 @@
+package Java50;
+
+import java.util.Stack;
+
+public class Fibonacci {
+    public static void main(String[] args) {
+        // Print Fibonacci Series
+        int num=4;
+       // System.out.println(fib(num));
+        int a=0;
+        int b=1;
+        int c;
+        int n=7;
+        int i=1;
+        while(i<=n){
+            System.out.println(a);
+            c=a+b;
+            a=b;
+            b=c;
+            i++;
+        }
+    }
+    // recursion method
+    public static int fib(int num){
+        if(num<=1){
+            return num;
+        }
+        return fib(num-1)+fib(num-2);
+    }
+}
